9975b2b3532e68dbf6f7768a1c36ba7dbaeae5f3
maksim.rameev@x5.ru <maksim.rameev@x5.ru>
2022-03-01 12:25:06 +0300

    fix

--- src/components/chat/chat.tmpl.ts
+++ src/components/chat/chat.tmpl.ts
@@ -2,9 +2,10 @@ export default `
   <div class="chats__list-chat chat">
     <div class="chat__user-photo"></div>
     <div class="chat__content">
-      <div class="chat__user-name">{{user}}</div>
+      <div class="chat__user-name">{{title}}</div>
       <div class="chat__message">{{message}}</div>
     </div>
+    <div>1</div>
     <time class="chat__time" datetime="{{datetime}}">{{time}}</time>
   </div>
 `;
